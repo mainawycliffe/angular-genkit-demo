ec940e02022cdf283832ea01ed60a0d13005d923
Maina Wycliffe <wmmaina7@gmail.com>
2025-05-14 11:16:33 +0300

    Implement Basic Genkit Flow for Poem Generation

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component, inject, resource, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { getFunctions, httpsCallable } from 'firebase/functions';
+import { FIREBASE_APP } from './app.config';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [FormsModule],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <div class="flex flex-col h-screen w-screen p-4 py-9 bg-gray-100">
+      <div class="flex flex-col flex-1 w-full max-w-[60rem] mx-auto">
+        <div class="flex flex-col flex-1 gap-y-2 border-b border-gray-300">
+          <div class="flex flex-row justify-between items-center">
+            <h1 class="text-2xl font-bold">Poem Generator</h1>
+          </div>
+          <div class="flex flex-col flex-1">
+            <h2 class="text-xl font-semibold mb-4">Generated Poem</h2>
+            <div
+              class="flex flex-col flex-1 justify-end py-4 overflow-y-auto text-lg font-semibold text-gray-800"
+            >
+              <!-- presserver line breaks -->
+              <p class="text-gray-800 whitespace-pre-line ">
+                {{ resource.isLoading() ? 'Generating...' : resource.value() }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row gap-2 w-full">
+          <input
+            [(ngModel)]="poemSubject"
+            type="text"
+            class="w-full p-2 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Subject for the poem to be generated"
+          />
+          <button
+            (click)="poem.set(poemSubject)"
+            class="p-2 px-4 bg-blue-500 text-white rounded min-w-max"
+            [disabled]="!poemSubject || resource.isLoading()"
+          >
+            Generate Poem
+          </button>
+        </div>
+      </div>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'web';
+  poemSubject = '';
+  poem = signal<string | undefined>(undefined);
+
+  firebaseApp = inject(FIREBASE_APP);
+
+  functions = getFunctions(this.firebaseApp);
+
+  resource = resource({
+    request: () => ({
+      subject: this.poem(),
+    }),
+    loader: async ({ request }) => {
+      if (!request.subject) {
+        return;
+      }
+      const callableFn = httpsCallable(this.functions, 'poem');
+      const res = await callableFn(request.subject);
+      return res.data;
+    },
+  });
 }
--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,10 +1,32 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FirebaseApp, initializeApp } from 'firebase/app';
 import { appRoutes } from './app.routes';
+
+export const FIREBASE_APP = new InjectionToken<FirebaseApp>('FIREBASE_APP');
+
+const initializeFirebaseApp = () =>
+  initializeApp({
+    apiKey: 'AIzaSyBPyQxfUqbZ3LcehijHEWUsTQN-lDGJ-ok',
+    authDomain: 'angular-genkit-demo.firebaseapp.com',
+    databaseURL: 'https://angular-genkit-demo-default-rtdb.firebaseio.com',
+    projectId: 'angular-genkit-demo',
+    storageBucket: 'angular-genkit-demo.firebasestorage.app',
+    messagingSenderId: '603362092209',
+    appId: '1:603362092209:web:a014f398175c653b60ee90',
+  });
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    {
+      provide: FIREBASE_APP,
+      useFactory: initializeFirebaseApp,
+    },
   ],
 };
